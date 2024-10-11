c918713eba860d4422cb2003461caa5837cffb15
db807 <db807@kent.ac.uk>
2024-10-11 11:05:43 +0100

    Update partSix.py

--- partSix.py
+++ partSix.py
@@ -1,3 +1,11 @@
 def main():
 age = input("what is your age?")
-student = input("are you a student?")
+student = input("are you a student? yes or no?")
+if age < 12 or age > 65
+print("£5")
+if age > 12 and age <65 and student = "yes"
+print("£8")
+if age > 12 and age <65 and student = "no"
+print("£10")
+
+main()
