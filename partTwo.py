6f0fefe67aa39d64333f8d167e4fcd7e426f3322
db807 <db807@kent.ac.uk>
2024-10-11 10:25:41 +0100

    Update partTwo.py

--- partTwo.py
+++ partTwo.py
@@ -1,1 +1,11 @@
+import random
+def main()
+x = int(input("pick a number between 1 and 10"))
+secret_number = random.randit(1 ,10)
+print("secret_number")
+if x = secret_number
+print ("congrats")
+if x != secert_number 
+print ("try again")
 
+main()
