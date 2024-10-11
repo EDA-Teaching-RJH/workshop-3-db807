1156c66cf2f23836f17890348af032677fdef37f
db807 <db807@kent.ac.uk>
2024-10-11 10:41:12 +0100

    Update partThree.py

--- partThree.py
+++ partThree.py
@@ -1,1 +1,14 @@
+def main():
+score = input("what is your numerical score?")
+if score > ("90") or score </= ("100")
+print("A")
+elif score > ("80") or score </= ("89")
+print("B")
+elif score > ("70") or score </= ("79")
+print("C")
+elif score > ("60") or score </= ("69")
+print("D")
+elif score </= ("59")
+print("F")
 
+main()
