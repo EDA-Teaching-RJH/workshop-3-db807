4c68b20ec1beb58d5c19e0cd2efbc92d9a15114d
db807 <db807@kent.ac.uk>
2024-10-11 10:55:40 +0100

    Update partFive.py

--- partFive.py
+++ partFive.py
@@ -1,1 +1,8 @@
+def main():
+day = input("What day of the week is it?:")
+if day == "monday" or day == "tuesday" or day == "wednesday" or day == "thursday" or day == "friday"
+print("it's a weekday.")
+elif day == "saurday" or "sunday"
+print("it's the wekend!")
 
+main()
