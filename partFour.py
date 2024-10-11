969351b20d9ce20356242d4d2f2667d767144cd5
db807 <db807@kent.ac.uk>
2024-10-11 10:50:28 +0100

    Update partFour.py

--- partFour.py
+++ partFour.py
@@ -1,1 +1,11 @@
+def main():
+x = input("enter a username?")
+user = int(input("user"))
+y = input("enter a password")
+password = int(input("password"))
+if x = user and y = password 
+print("access granted")
+if x != user and or y != password
+print(" Error, access denied")
 
+main()
