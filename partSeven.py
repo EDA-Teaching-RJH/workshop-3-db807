8a11be088cf0e3efaef96f05c9a414beb14e8c84
db807 <db807@kent.ac.uk>
2024-10-11 11:31:58 +0100

    Update partSeven.py

--- partSeven.py
+++ partSeven.py
@@ -1,1 +1,32 @@
+import math
+def main():
+num1 = input("enter a number:")
+num2 = input("enter another number:")
+operation = input("enter an operation ie; +, -, *, /, ^, %")
+match someting:
+case "+":
+  result = num1 + num2
+  print(f"{num1} + {num2} = {result}")
+case "-":
+  result2 = num1 - num2
+  print(f"{num1} - {num2} = {result2}")
+case "*":
+  result3 = num1 * num2
+  print(f"{num1} * {num2} = {result3}")
+case "/":
+  result4 = num1 / num2
+  print(f"{num1} / {num2} = {result4}")
+case "^":
+  result5 = num1 ^ num2
+  print(f"{num1} ^ {num2} = {result5}")
+case "%":
+  result6 = num1 % num2
+  print(f"{num1} % {num2} = {result6}")
+case "something":
+  print("quitting application")
+  break
 
+if num 2 = "0" and operation = "/"
+print "error"
+
+main()
