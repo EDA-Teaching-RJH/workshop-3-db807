224e6cdd99ec77902c99bbe670f33f58fbcb057e
db807 <db807@kent.ac.uk>
2024-10-15 22:33:45 +0100

    Update partEight.py

--- partEight.py
+++ partEight.py
@@ -1,1 +1,20 @@
-
+def main():
+print("Welcome traveller, to the dungeons of Fear and Hunger.")
+name = input("what is your name?")
+def name
+y = input("Alright (name) will your weapon be the axe or the sword?")
+if y = axe
+print("You got the axe.")
+elif y = sword
+print("You got the sword.")
+elif y != sword or axe 
+print("pick one") , break
+inventory = []
+inventory.append("(y)")
+print("You see the large stone walls of the dungeon, there are barrels outside, but you hear barking in the distance.")
+choice1 input("search barrels or hide?")
+if choice1 = search barrels
+print"(You search the barrels and found a mouldy bread.")
+inventory.append("(choice1)")
+elif choice1 = hide 
+print"(You hide in the debris."
