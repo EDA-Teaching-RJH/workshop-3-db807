6525b7f3c846b2572e36ee8a2ca5ddf9cff1fda0
db807 <db807@kent.ac.uk>
2024-10-16 17:39:34 +0100

    Update partEight.py

--- partEight.py
+++ partEight.py
@@ -29,6 +29,21 @@ else:
 input("Heads or tails?")
 if y = 1 break 
 if y = 0 print("you escaped it's grasp.")
-print(" As you turn to strike the beast dirt goes in your eye obscuring vision."
-input(" pick a number from 1-5 to determine where your attack strikes.")
-im
+print(" As you turn to strike the beast dirt goes in your eye obscuring vision.")
+import random
+atk = int(input(" pick a number from 1-5 to determine where your attack strikes."))
+secret_number = random.randit(1 ,5)
+print("secret number")
+if atk = secret_numnber
+print("The beast goes down in one fell strike, you got the wolf skin.") and inventory.append("wolf skin")
+if atk != secret_number 
+print("You are torn to pieces.") and break
+print("As you lick your wounds , you enter thedungeon and encounter a shady figure.")
+print(" Hello traveller, I am Pierre, I will give you a powerful ring in exchange for the wolf skin. what do you say?")
+print("((something about this man sends shivers down your spine but the ring looks priceless.))")
+D = int(input(" Deal or no deal:")")
+if D = Deal
+print(" you get the ring and it glows with mystical arura.") and invetory.remove("wolf skin") and inventory.append("ring")
+if D = no deal 
+print(" The merchant stabs you and you feel him rob you of everything as the world fades to black.") break
+main()
