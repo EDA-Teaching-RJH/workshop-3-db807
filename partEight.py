b767823104b3d00c33a5ca2bc72fe50143712ce3
db807 <db807@kent.ac.uk>
2024-10-16 17:20:28 +0100

    Update partEight.py

--- partEight.py
+++ partEight.py
@@ -14,7 +14,21 @@ inventory.append("(y)")
 print("You see the large stone walls of the dungeon, there are barrels outside, but you hear barking in the distance.")
 choice1 input("search barrels or hide?")
 if choice1 = search barrels
-print"(You search the barrels and found a mouldy bread.")
+print("You search the barrels and found a mouldy bread.")
 inventory.append("(choice1)")
 elif choice1 = hide 
-print"(You hide in the debris."
+print("You hide in the debris.")
+print("A foul beast snares it sharp fangs at you as you draw your weapon.")
+print("it lunges foward with great strength.")
+import random
+randomHT= random.randint(0,1)
+if(randomHT==1):
+    print("Head")
+else:
+    print("Tail")
+input("Heads or tails?")
+if y = 1 break 
+if y = 0 print("you escaped it's grasp.")
+print(" As you turn to strike the beast dirt goes in your eye obscuring vision."
+input(" pick a number from 1-5 to determine where your attack strikes.")
+im
